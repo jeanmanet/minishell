3bd8111e41a9869fbdc8d28f9f815efc121b1ba8
Jean MANET <jmanet@Jeans-MacBook-Pro.local>
2023-03-18 21:21:34 +0100

    Leaks for AST OK

--- srcs/utils/mem_utils.c
+++ srcs/utils/mem_utils.c
@@ -6,7 +6,7 @@
 /*   By: jmanet <jmanet@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 09:51:45 by jmanet            #+#    #+#             */
-/*   Updated: 2023/03/18 21:00:46 by jmanet           ###   ########.fr       */
+/*   Updated: 2023/03/18 21:19:12 by jmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,18 +28,27 @@ void	free_cmd_node(t_ast_node	*cmd_node)
 	free(cmd_node->content->cmd->args);
 	free(cmd_node->content->cmd);
 	free(cmd_node->content);
+	free(cmd_node);
+}
+
+void	free_pipe_node(t_ast_node	*pipe_node)
+{
+	if (pipe_node->content->pipe->left->type == AST_PIPE)
+		free_pipe_node(pipe_node->content->pipe->left);
+	else
+		free_cmd_node(pipe_node->content->pipe->left);
+	free_cmd_node(pipe_node->content->pipe->right);
+	free(pipe_node->content->pipe);
+	free(pipe_node->content);
+	free(pipe_node);
 }
 
 void	free_ast(t_ast	*ast_tree)
 {
 	if (ast_tree->root->type == AST_CMD)
-	{
 		free_cmd_node(ast_tree->root);
-		// free(ast_tree->root->content->cmd->args);
-		// free(ast_tree->root->content->cmd);
-		// free(ast_tree->root->content);
-	}
-	free(ast_tree->root);
+	else
+		free_pipe_node(ast_tree->root);
 	ast_tree->root = NULL;
 }
 
@@ -48,6 +57,4 @@ void	free_mem(t_data *data)
 	free(data->command_line);
     free_tokens(&data->token_list);
     free_ast(data->commands_tree);
-    // if (data->command->args)
-    //     free(data->command->args);
 }
