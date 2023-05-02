e9d1acbf1fb49e3c2ecbf53d48dffefff372a9fc
Jean MANET <jmanet@Jeans-MacBook-Pro.local>
2023-05-02 14:52:45 +0200

    patch execute pipe node for cat | cat | ls cmd

--- srcs/exec/exec_ast.c
+++ srcs/exec/exec_ast.c
@@ -6,7 +6,7 @@
 /*   By: jmanet <jmanet@student.42nice.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/10 14:05:35 by jmanet            #+#    #+#             */
-/*   Updated: 2023/04/28 18:43:09 by jmanet           ###   ########.fr       */
+/*   Updated: 2023/05/02 14:42:26 by jmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,35 +24,26 @@ int	execute_cmd_node(t_ast_node *node, t_data *data)
 
 int	execute_left_node(t_ast_node *left, int *pipe_fd, t_data *data)
 {
-	pid_t	pid;
-
-	pid = fork();
-	if (pid == -1)
-	{
-		perror("Fork");
-		return (-1);
-	}
-	else if (pid == 0)
-	{
-		close(pipe_fd[0]);
-		dup2(pipe_fd[1], STDOUT_FILENO);
-		if (left->type == AST_CMD)
-			execute_cmd_node(left, data);
-		else
-			execute_pipe_node(left, data);
-		exit(0);
-	}
-	waitpid(pid, NULL, 0);
+	close(pipe_fd[0]);
+	dup2(pipe_fd[1], STDOUT_FILENO);
+	if (left->type == AST_CMD)
+		execute_cmd_node(left, data);
+	else
+		execute_pipe_node(left, data);
+	exit(0);
 	return (0);
 }
 
 int	execute_right_node(t_ast_node *right, int *pipe_fd, t_data *data)
 {
 	int	returnval;
+	int	savefd;
 
+	savefd = dup(STDIN_FILENO);
 	close(pipe_fd[1]);
 	dup2(pipe_fd[0], STDIN_FILENO);
 	returnval = execute_cmd_node(right, data);
+	dup2(savefd, STDIN_FILENO);
 	return (returnval);
 }
 
@@ -60,28 +51,25 @@ int	execute_pipe_node(t_ast_node *node, t_data *data)
 {
 	int		pipe_fd[2];
 	int		ret;
-	pid_t	pid2;
+	pid_t	pid;
 
 	if (pipe(pipe_fd) == -1)
 	{
 		perror("Pipe");
 		return (-1);
 	}
-	ret = execute_left_node(node->content->pipe->left, pipe_fd, data);
-	pid2 = fork();
-	if (pid2 == -1)
+	pid = fork();
+	if (pid == -1)
 	{
 		perror("Fork");
 		return (-1);
 	}
-	else if (pid2 == 0)
-	{
-		ret = execute_right_node(node->content->pipe->right, pipe_fd, data);
-		exit(0);
-	}
+	else if (pid == 0)
+		ret = execute_left_node(node->content->pipe->left, pipe_fd, data);
+	ret = execute_right_node(node->content->pipe->right, pipe_fd, data);
 	close(pipe_fd[0]);
 	close(pipe_fd[1]);
-	waitpid(pid2, NULL, 0);
+	waitpid(pid, NULL, 0);
 	return (ret);
 }
 
